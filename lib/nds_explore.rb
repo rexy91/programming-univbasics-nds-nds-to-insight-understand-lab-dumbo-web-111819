7981dae56b0fff077da33bb72acbbd4edd3caf04
Yu Hao Ye <rxye1991@gmail.com>
2019-11-16 19:59:32 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ end
 def print_first_directors_movie_titles
   i = 0
 while i < 6 do 
-print directors_database[0][:movies][i][:title]
+puts directors_database[0][:movies][i][:title]
 i += 1 
 end
 end
